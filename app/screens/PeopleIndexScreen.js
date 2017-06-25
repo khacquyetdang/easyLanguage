390f651acc027a9f2f1cc202e316f33f639a6666
mrdang <khacquyet.dang@gmail.com>
2017-06-25 22:39:11 +0200

    add navigation add detail screeen

---
+++ app/screens/PeopleIndexScreen.js
@@ -0,0 +1,98 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  ListView,
+  View,
+  TouchableOpacity
+} from 'react-native';
+
+import _ from 'lodash'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import StatusBarBackground from '../components/StatusBarBackground'
+import ViewContainer from '../components/ViewContainer'
+
+
+const people = [{firstName: "Dang", lastName: "Khac Quyet"},
+                {firstName: "TRAN", lastName: "Thi Thu Hong"},
+                {firstName: "Dang", lastName: "Leon"}
+              ]
+
+export default class PeopleIndexScreen extends Component {
+
+  static navigationOptions = {
+      title: 'List people',
+    };
+  constructor() {
+      super();
+      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+      this.state = {
+        dataSource: ds.cloneWithRows(people),
+      };
+    }
+
+  render() {
+    return (
+    <ViewContainer>
+        <ListView
+          style={{marginTop:100}}
+          dataSource={this.state.dataSource}
+          renderRow={(person) => {return this._renderPersonRow(person)}}
+      />
+    </ViewContainer>
+    );
+  }
+
+  _renderPersonRow(person) {
+
+    const { navigate } = this.props.navigation;
+    console.log(person)
+    return (
+      <TouchableOpacity  style={styles.personRow} onPress={() => navigate('PeopleShow', {person : person})}>
+        <Text style={styles.personName}>
+          {`${person.firstName} ${person.lastName}`}
+        </Text>
+        <View style={{flex: 1}} />
+        <Icon name="chevron-right" size={15} style={styles.personMoreIcon}/>
+      </TouchableOpacity>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    marginTop:100,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+
+  personRow: {
+    flexDirection: "row",
+    margin:10,
+    justifyContent: "flex-start",
+    alignItems: "center"
+  },
+
+  personName: {
+    fontSize: 25,
+    textAlign: 'center',
+    margin: 10,
+  },
+
+  personMoreIcon: {
+    color: "green",
+    height:30,
+    width: 30
+  }
+});
+
+module.exports = PeopleIndexScreen;
--- index.android.js
+++ index.android.js
@@ -5,90 +5,29 @@
  */
 
 import React, { Component } from 'react';
+
+import {
+  StackNavigator,
+} from 'react-navigation';
+
 import {
   AppRegistry,
   StyleSheet,
-  Text,
-  ListView,
-  View,
-  TouchableOpacity
+  Navigator
 } from 'react-native';
 
 import _ from 'lodash'
-import Icon from 'react-native-vector-icons/FontAwesome'
-import StatusBarBackground from './app/components/StatusBarBackground'
-import ViewContainer from './app/components/ViewContainer'
+import PeopleIndexScreen from "./app/screens/PeopleIndexScreen"
+import PeopleShowScreen from "./app/screens/PeopleShowScreen"
 
 
-const people = [{firstName: "Dang", lastName: "Khac Quyet"},
-                {firstName: "TRAN", lastName: "Thi Thu Hong"},
-                {firstName: "Dang", lastName: "Leon"}
-              ]
-
-export default class easyLanguage extends Component {
-
-  constructor() {
-      super();
-      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-      this.state = {
-        dataSource: ds.cloneWithRows(people),
-      };
-    }
-
-  render() {
-    return (
-    <ViewContainer>
-        <ListView
-          style={{marginTop:100}}
-          dataSource={this.state.dataSource}
-          renderRow={(person) => {return this._renderPersonRow(person)}}
-      />
-    </ViewContainer>
-    );
-  }
-
-  _renderPersonRow(person) {
-
-    console.log(person)
-    return (
-      <TouchableOpacity  style={styles.personRow} onPress={(event) => console.log("person")}>
-        <Text style={styles.personName}>
-          {`${person.firstName} ${person.lastName}`}
-        </Text>
-        <View style={{flex: 1}} />
-        <Icon name="chevron-right" size={30} style={styles.personMoreIcon}/>
-      </TouchableOpacity>
-    )
-  }
-}
+const easyLanguage = StackNavigator({
+  PeopleIndex: { screen: PeopleIndexScreen },
+  PeopleShow: { screen: PeopleShowScreen }
+});
 
 const styles = StyleSheet.create({
-
-  container: {
-    marginTop:100,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-
-  personRow: {
-    flexDirection: "row",
-    margin:10,
-    justifyContent: "flex-start",
-    alignItems: "center"
-  },
-
-  personName: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-
-  personMoreIcon: {
-    color: "green",
-    height:30,
-    width: 30
+  navigatorStyles: {
   }
 });
 

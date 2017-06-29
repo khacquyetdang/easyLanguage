63388116ad01a180a66190d2a67925d01bdcf033
mrdang <khacquyet.dang@gmail.com>
2017-06-29 08:59:58 +0200

    when user click on the wrong answer, alert user with the red button text that the answer is Wrong when user click on the right answer, alert user with the green text that the answer is rigth @TODO: add the list of question, answser to set an demo of games

--- app/screens/TestVocabularyScreen.js
+++ app/screens/TestVocabularyScreen.js
@@ -68,171 +68,173 @@ class TestVocabularyScreen extends React.Component {
     console.log("render next")
     if (this.state.answerClicked)
     {
-        if (this.state.currentAnswerMatched)
-        {
-          return (
-            <View style={styles.rowContainer}>
 
-              <TouchableHighlight style={styles.buttonCheck}
+      if (this.state.currentAnswerMatched === true)
+      {
+        return (
+          <View style={styles.rowContainer}>
+
+            <TouchableHighlight style={styles.buttonCheck}
               onPress={() => { this.goToNextTest()}}>
 
-                  <Text>Correct answer, go next</Text>
-              </TouchableHighlight>
+              <Text>Correct answer, go next</Text>
+            </TouchableHighlight>
 
-            </View>
-          )
-        }
-        else {
-          return
-              (
-                <View style={styles.rowContainer}>
-
-                  <TouchableHighlight style={styles.buttonWrong}
-                  onPress={() => { }}>
-
-                      <Text>Wrong answer, go next</Text>
-                      <View style={{backgroundColor : 'black', height: 50, width: 60}}></View>
-                  </TouchableHighlight>
-
-                </View>
-              )
-        }
+          </View>
+        )
       }
-      return <View style={{backgroundColor : 'blue', height: 50, width: 60}}></View>;
-  }
-  render() {
-     const { params } = this.props.navigation.state;
-    return (
-              <View style={styles.columnContainer}>
+      else {
+        return (
+          <View style={styles.rowContainer}>
 
-              <View style={styles.question}>
-                <Text style={styles.textquestion}>
-                  {'Choisir l\'image correspondant à un oiseau?'}
-                </Text>
-                </View>
+            <TouchableHighlight style={styles.buttonWrongAnswer}
+              onPress={() => {}}>
+
+              <Text style={styles.textWrongAnswer}>Wrong answer, try again</Text>
+            </TouchableHighlight>
+          </View>
+        )
+      }
+    }
+    return <View></View>;
+    }
+    render() {
+      const { params } = this.props.navigation.state;
+      return (
+        <View style={styles.columnContainer}>
+
+          <View style={styles.question}>
+            <Text style={styles.textquestion}>
+              {'Choisir l\'image correspondant à un oiseau?'}
+            </Text>
+          </View>
 
 
-              <View style={styles.rowContainer}>
-                  <View style={styles.column}>
-                  <TouchableHighlight
-                  onPress={() => this.onImageClick(0)}>
-                    <Image
-                      style={styles.imageAnswerStyle}
-                      source={require('../../image/burger.jpg')}
-                    />
-                    </TouchableHighlight>
-                  </View>
-
-                  <View style={styles.column}>
-                  <TouchableHighlight
-                    onPress={() => this.onImageClick(1)}>
-                  <Image
-                    style={styles.imageAnswerStyle}
-                    source={require('../../image/car.jpg')}
+          <View style={styles.rowContainer}>
+            <View style={styles.column}>
+              <TouchableHighlight
+                onPress={() => this.onImageClick(0)}>
+                <Image
+                  style={styles.imageAnswerStyle}
+                  source={require('../../image/burger.jpg')}
                   />
-                  </TouchableHighlight>
-                  </View>
+              </TouchableHighlight>
+            </View>
 
-                </View>
-
-                <View style={styles.rowContainer}>
-                  <View style={styles.column}>
-                  <TouchableHighlight
-                  onPress={() => this.onImageClick(2)}>
-                  <Image
-                    style={styles.imageAnswerStyle}
-                    source={require('../../image/dog.jpg')}
-                    />
-                  </TouchableHighlight>
-                  </View>
-
-                  <View style={styles.column}>
-                  <TouchableHighlight
-                  onPress={() => this.onImageClick(3)}>
-                  <Image
-                    style={styles.imageAnswerStyle}
-                    source={require('../../image/oiseau.jpg')}
+            <View style={styles.column}>
+              <TouchableHighlight
+                onPress={() => this.onImageClick(1)}>
+                <Image
+                  style={styles.imageAnswerStyle}
+                  source={require('../../image/car.jpg')}
                   />
-                  </TouchableHighlight>
-                  </View>
+              </TouchableHighlight>
+            </View>
 
-                </View>
+          </View>
 
-              { this._renderNext() }
-              </View>
-    );
+          <View style={styles.rowContainer}>
+            <View style={styles.column}>
+              <TouchableHighlight
+                onPress={() => this.onImageClick(2)}>
+                <Image
+                  style={styles.imageAnswerStyle}
+                  source={require('../../image/dog.jpg')}
+                  />
+              </TouchableHighlight>
+            </View>
+
+            <View style={styles.column}>
+              <TouchableHighlight
+                onPress={() => this.onImageClick(3)}>
+                <Image
+                  style={styles.imageAnswerStyle}
+                  source={require('../../image/oiseau.jpg')}
+                  />
+              </TouchableHighlight>
+            </View>
+
+          </View>
+
+          { this._renderNext() }
+        </View>
+      );
+    }
   }
-}
 
- var width = Dimensions.get('window').width; //full width
- var columnWidth = width / 2 - 30;
- var rowHeight = width / 2 - 10;
- var imageSize = width / 3;
+  var width = Dimensions.get('window').width; //full width
+  var columnWidth = width / 2 - 30;
+  var rowHeight = width / 2 - 10;
+  var imageSize = width / 3;
 
-const styles = StyleSheet.create({
+  const styles = StyleSheet.create({
 
-  container: {
-    flex: 1,
-    backgroundColor: 'violet',
-  },
+    container: {
+      flex: 1,
+      backgroundColor: 'violet',
+    },
 
-  question : {
-    marginTop:10,
-    marginLeft: 20,
-    marginRight: 20,
-  },
-  textquestion : {
-    fontWeight : 'bold',
-    fontSize: 25
-  },
+    question : {
+      marginTop:10,
+      marginLeft: 20,
+      marginRight: 20,
+    },
+    textquestion : {
+      fontWeight : 'bold',
+      fontSize: 25
+    },
 
-  columnContainer: {
-    flex:1,
-    flexDirection: 'column',
-    alignItems: 'stretch'
-  },
-  rowContainer: {
-    flexDirection: 'row',
-    height: rowHeight,
-    margin: 5
-      },
+    columnContainer: {
+      flex:1,
+      flexDirection: 'column',
+      alignItems: 'stretch'
+    },
+    rowContainer: {
+      flexDirection: 'row',
+      height: rowHeight,
+      margin: 5
+    },
 
-  column: {
-    margin: 10,
-    flex:1,
-    alignItems: 'center',
-    borderColor: 'blue',
-    borderWidth: 2
-  },
+    column: {
+      margin: 10,
+      flex:1,
+      alignItems: 'center',
+      borderColor: 'blue',
+      borderWidth: 2
+    },
 
-  imageAnswerStyle: {
-    margin: 2,
-    width: imageSize,
-    height: imageSize
-  },
+    imageAnswerStyle: {
+      margin: 2,
+      width: imageSize,
+      height: imageSize
+    },
 
-  buttonCheck: {
-    flex: 1,
-    backgroundColor: 'green',
-    alignSelf: 'stretch',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 50,
-    margin: 10,
-    borderWidth:1,
-  },
+    buttonCheck: {
+      flex: 1,
+      backgroundColor: 'green',
+      alignSelf: 'stretch',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 50,
+      margin: 10,
+      borderWidth:1,
+    },
 
-  buttonWrong: {
-    flex: 1,
-    backgroundColor: 'red',
-    alignSelf: 'stretch',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 50,
-    margin: 10,
-    borderWidth:1,
-  }
-});
+    buttonWrongAnswer: {
+      flex: 1,
+      backgroundColor: 'red',
+      alignSelf: 'stretch',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 50,
+      margin: 10,
+      borderWidth:1,
+    },
+
+    textWrongAnswer: {
+      color: 'white'
+    }
+  });
 
 
-module.exports = TestVocabularyScreen;
+  module.exports = TestVocabularyScreen;
